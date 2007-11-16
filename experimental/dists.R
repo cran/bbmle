d57d09f81b6f2573d660cd90087bcc46a0038077
Ben Bolker <bolker@zoo.ufl.edu>
2007-11-16 13:04:21 +0000

    version 0.8.2

--- experimental/dists.R
+++ experimental/dists.R
@@ -8,10 +8,57 @@ sbinom <- function(prob,size) {
        SD=sqrt(size*prob*(1-prob)))
 }
 
-abinom <- function(prob,size) {
-  list(
-       Formula=expression(choose(size,x)*prob^x*(1-prob)^(size-x)),
-       Formula.log=expression(lgamma(size+1)-lgamma(x+1)-lgamma(size-x+1)+
-           x*log(prob) + (size-x)*log(1-prob)))
+abinom <- function() {
+  list(params=c("prob","size"),
+       fstr = "choose(size,x)*prob^x*(1-prob)^(size-x)",
+       lfstr = "lgamma(size+1)-lgamma(x+1)-lgamma(size-x+1)+\
+x*log(prob) + (size-x)*log(1-prob)")
 }
 
+apois <- function() {
+  list(params=c("lambda"),
+       fstr = "lambda^x*exp(-lambda)/factorial(x)",
+       lfstr = "x*log(lambda)-lambda-lgamma(x+1)")
+}
+
+fsubs <- function(lf,pars,x,...) {
+  upars = as.list(...)
+  for (i in 1:length(pars)) {
+    lf = gsub(pars[i],deparse(upars[[i]]),lf)
+  }
+  gsub("\\bx\\b",x,lf)
+}
+
+derivfun = function(formula,params) {
+  f6 = formula[[3]] ## extract the RHS
+  xvar = as.character(formula[[2]]) ## extract the LHS
+  fn = do.call(gsub("^d","a",as.character(f6[[1]])),list()) ## call a-function
+  deriv(parse(text=fsubs(fn$lfstr,fn$params,xvar,f6[-1])),
+        params,function.arg=TRUE)
+}
+
+lf1 = abinom(a^2+b,s)$Formula.log
+deriv(lf1,"a")
+f2 = z~dbinom(1/(1+exp(-a)),size=N)
+f3 = f2[[3]]
+f3[[1]] = quote(abinom)
+f4 = eval(f3)
+deriv(f4$Formula.log,"a")
+
+## for example: start with this formula (from ?mle)
+
+f5 = y~dpois(lambda=exp(lymax)/(1+x/exp(lhalf)))
+params = c("lymax","lhalf")
+d1 = derivfun(f5,params)
+y=c(2:7)
+d1(1,1)
+
+
+
+## arbitrary parameter ordering?
+## still need to wrap function to take negative sum
+## using gradient within optim stuff
+##  (seems a waste to have a separate function ...)
+
+## could use gradient attribute directly in nlm
+## could use hessian=TRUE for nlm, nlminb ... (is it worth it?)
